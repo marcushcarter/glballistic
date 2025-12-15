8fce1fc50e782aa3c6c18a41710a63061b856e80
Marcus <marcus.carter@rogers.com>
2025-12-15 10:59:40 -0500

    examples

---
+++ examples/hello_triangle/main.cpp
@@ -0,0 +1,13 @@
+#include <glballistic/Buffer.h>   // your header-only library
+#include <GLFW/glfw3.h>
+#include <glad/glad.h>
+#include <iostream>
+
+int main() {
+    // GLFW + GLAD init...
+
+    std::cout << "Hello, World" << std::endl;
+
+    while(true) {}
+    return 0;
+}
